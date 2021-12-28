684bb5d4d29f36bb58cc7f97ef3adea672c78891
ShinJiHyeong <bs9992@nara.co.kr>
2021-12-28 09:49:08 +0900

    프로젝트 구성

---
+++ contacts/src/test/java/mokoko/contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package mokoko.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
