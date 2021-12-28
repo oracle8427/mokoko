4c0e2953ce7220a8d2ee64e6a4a02461bb281c5c
ShinJiHyeong <bs9992@nara.co.kr>
2021-12-28 11:51:37 +0900

    프로젝트 설정 변경
    - spring-boot-maven-plugin 의존성 제거 (불필요)
    - contacts 모듈의 부모 pom artifactId 수정 (잘못 참조)

--- core/src/main/java/mokoko/CoreApplication.java
+++ core/src/main/java/mokoko/CoreApplication.java
@@ -1,9 +1,7 @@
-package mokoko.core;
+package mokoko;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
