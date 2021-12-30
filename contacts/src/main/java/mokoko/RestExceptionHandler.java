a89ef4a221f3b68cf747ae180cf58b1111644564
ShinJiHyeong <bs9992@nara.co.kr>
2021-12-30 20:33:40 +0900

    컨트롤러 예외전략 및 JacksonUtil 클래스 추가
    Controller 예외를 RestExceptionHandler 클래스에 처리 위임

---
+++ contacts/src/main/java/mokoko/RestExceptionHandler.java
@@ -0,0 +1,58 @@
+package mokoko;
+
+import mokoko.error.BadRequestException;
+import mokoko.error.NotFoundException;
+import mokoko.error.UnauthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	@ExceptionHandler
+	public ResponseEntity<?> handleException(Exception e) {
+		log.error(e.getMessage(), e);
+
+		Map<String, Object> responseMap = new HashMap<>();
+		try {
+			String message = e.getMessage();
+			String stacktrace = getStackTrace(e);
+			if (!log.isDebugEnabled() ||
+					(message != null && message.length() > 0 && message.contains("Error querying database"))) {
+				message = "";
+				stacktrace = "";
+			}
+			responseMap.put("message", message);
+			responseMap.put("stacktrace", stacktrace);
+		} catch (Exception localException) {
+			log.error("Error at RestExceptionHandler.handleException", localException);
+		}
+
+		if (e instanceof BadRequestException) {
+			return new ResponseEntity<>(responseMap, HttpStatus.BAD_REQUEST);
+		} else if (e instanceof NotFoundException) {
+			return new ResponseEntity<>(responseMap, HttpStatus.NOT_FOUND);
+		} else if (e instanceof UnauthorizedException) {
+			return new ResponseEntity<>(responseMap, HttpStatus.UNAUTHORIZED);
+		}
+		return new ResponseEntity<>(responseMap, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	private String getStackTrace(Throwable thrown) {
+		StringWriter sw = new StringWriter();
+		thrown.printStackTrace(new PrintWriter(sw));
+		return sw.toString();
+	}
+
+}
---
+++ core/src/main/java/mokoko/error/BadRequestException.java
@@ -0,0 +1,23 @@
+package mokoko.error;
+
+public class BadRequestException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public BadRequestException() {
+		super();
+	}
+
+	public BadRequestException(String message) {
+		super(message);
+	}
+
+	public BadRequestException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BadRequestException(Throwable cause) {
+		super(cause);
+	}
+
+}
